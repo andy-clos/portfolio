d698b12c0f527d55eb031c6940ddda5a9baa21f0
Andy <andyclos@student.usm.my>
2024-10-06 18:09:37 +0800

    update experience

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,19 +3,51 @@ import React from "react";
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
-import skill1image from "../../assets/skills/html.png";
-import skill2image from "../../assets/skills/css.png";
-import skill3image from "../../assets/skills/react.png";
-import skill4image from "../../assets/skills/figma.png";
+import skill1image from "../../assets/skills/react.png";
+import skill2image from "../../assets/skills/bootstrap.png";
+import skill3image from "../../assets/skills/html.png";
+import skill4image from "../../assets/skills/css.png";
+import skill5image from "../../assets/skills/vscode.png";
+import skill6image from "../../assets/skills/github.png";
+import skill7image from "../../assets/skills/figma.png";
+import skill8image from "../../assets/skills/tailwindcss.png";
+import skill9image from "../../assets/skills/git.png";
+import skill10image from "../../assets/skills/gitlab.png";
+import skill11image from "../../assets/skills/python.png";
+import skill12image from "../../assets/skills/cpp.png";
+import skill13image from "../../assets/skills/java.png";
+import skill14image from "../../assets/skills/javascript.png";
+import skill15image from "../../assets/skills/typescript.png";
+import skill16image from "../../assets/skills/anaconda.png";
+import skill17image from "../../assets/skills/firebase.png";
+import skill18image from "../../assets/skills/mysql.png";
+import skill19image from "../../assets/skills/oracle.png";
+import skill20image from "../../assets/skills/jira.png";
 import history1image from "../../assets/history/vitrox.png";
 import history2image from "../../assets/history/css.png";
 
 export const Experience = () => {
   const skillImages = {
-    'html.png': skill1image,
-    'css.png': skill2image,
-    'react.png': skill3image,
-    'figma.png': skill4image
+    'react.png': skill1image,
+    'bootstrap.png': skill2image,
+    'html.png': skill3image,
+    'css.png': skill4image,
+    'vscode.png': skill5image,
+    'github.png': skill6image,
+    'figma.png': skill7image,
+    'tailwindcss.png': skill8image,
+    'git.png': skill9image,
+    'gitlab.png': skill10image,
+    'python.png': skill11image,
+    'cpp.png': skill12image,
+    'java.png': skill13image,
+    'javascript.png': skill14image,
+    'typescript.png': skill15image,
+    'anaconda.png': skill16image,
+    'firebase.png': skill17image,
+    'mysql.png': skill18image,
+    'oracle.png': skill19image,
+    'jira.png': skill20image
   };
 
   const historyImages = {
@@ -50,7 +82,8 @@ export const Experience = () => {
                   alt={`${historyItem.organisation} Logo`}
                 />
                 <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <h3><b>{`${historyItem.role},`}</b></h3>
+                  <h3><b>{`${historyItem.organisation}`}</b></h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
